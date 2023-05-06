1c2e17f18c8edb4cba843e42c8de4cb0ff32e4ec
Tapan Karangiya <99003457+tapan111@users.noreply.github.com>
2023-05-06 23:11:51 +0530

    Update uart_controller.v

--- uart_controller.v
+++ uart_controller.v
@@ -1,11 +1,11 @@
-`include "baudRateGenerator.sv"
+`include "baudRateGenerator.v"
 `ifdef UART_TX_ONLY
 `include "uart_tx_controller.v"
 `elsif UART_RX_ONLY
-`include "uart_rx_controller.sv"
+`include "uart_rx_controller.v"
 `else
 `include "uart_tx_controller.v"
-`include "uart_rx_controller.sv"
+`include "uart_rx_controller.v"
 `endif
 
 
